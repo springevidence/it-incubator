370765bc0bf2a1500409fcf591c614583a218971
springevidence <exshipbuilder1@gmail.com>
2023-04-05 15:42:11 +0300

    fcc | done basic task on variables and calculations with them

---
+++ freeCodeCamp/06.js
@@ -0,0 +1,59 @@
+//Initializing Variables with the Assignment Operator
+var myFirstName = 'Vika';
+var myLastName = 'Tiurina';
+
+//Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact);
+
+//Add Two Numbers with JavaScript
+const sum = 10 + 10;
+
+//Subtract One Number from Another with JavaScript
+const difference = 45 - 23;
+
+//Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+//Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+//Increment a Number with JavaScript
+let myVar = 87;
+myVar++;
+
+//Decrement a Number with JavaScript
+myVar--;
+
+//Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+const myDecimal = 4.3;
+
+//Finding a Remainder in JavaScript
+const remainder = 11 % 3;
+
+//
