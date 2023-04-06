02e79324cb75087635505c659ac582213a3491d6
springevidence <exshipbuilder1@gmail.com>
2023-04-06 10:17:48 +0300

    fcc | done multi-D arrays task

---
+++ freeCodeCamp/08.js
@@ -0,0 +1,24 @@
+//Store Multiple Values in one Variable using JavaScript Arrays
+const myArray = ["bread", 7];
+
+//Nest one Array within Another Array
+const myArr = [[1, 2, 3]];
+
+//Access Array Data with Indexes
+const myArray1 = [50, 60, 70];
+const myData = myArray1[0];
+
+//Modify Array Data With Indexes
+const myArray2 = [18, 64, 99];
+myArray2[0] = 45;
+
+//Access Multi-Dimensional Arrays With Indexes
+const myArray3 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+const data = myArray3[2][1];
+
