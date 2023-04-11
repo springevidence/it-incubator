8067a40def7f125ea4f2a56ccad1adb219d622b4
springevidence <exshipbuilder1@gmail.com>
2023-04-11 20:41:27 +0300

    fcc | done assigment with a returned value task

--- freeCodeCamp/09.js
+++ freeCodeCamp/09.js
@@ -62,3 +62,9 @@ function addFive() {
 addThree();
 addFive();
 
+// Assignment with a Returned Value
+let processed = 0;
+function processArg(num) {
+    return (num + 3) / 5;
+}
+processed = processArg(7);
