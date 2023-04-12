c8b80e036d08f28fb482dc95e0a2ae371024ffb4
springevidence <exshipbuilder1@gmail.com>
2023-04-12 16:43:23 +0300

    fcc | done conditional logic task

--- freeCodeCamp/09.js
+++ freeCodeCamp/09.js
@@ -83,3 +83,12 @@ function welcomeToBooleans() {
     return true; 
 }
 
+//Use Conditional Logic with If Statements
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+  }
+trueOrFalse(true);
+trueOrFalse(false);
