313fb91bd7fb092c764338a4a0469a0a79a0f1cd
springevidence <exshipbuilder1@gmail.com>
2023-04-14 11:43:08 +0300

    fcc | done replacing if else chains with switch

--- freeCodeCamp/12.js
+++ freeCodeCamp/12.js
@@ -63,3 +63,28 @@ function sequentialSizes(val) {
   return answer;
 }
 sequentialSizes(1);
+
+//Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+chainToSwitch(7);
