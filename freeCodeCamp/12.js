63f1cfedd6f49ce7225d478940c3b750478a78ca
springevidence <exshipbuilder1@gmail.com>
2023-04-14 11:05:49 +0300

    fcc | done adding a default option in switch statements task

--- freeCodeCamp/12.js
+++ freeCodeCamp/12.js
@@ -18,3 +18,24 @@ function caseInSwitch(val) {
   return answer;
 }
 console.log(caseInSwitch(2));
+
+//Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case 'a':
+      answer = "apple";
+      break;
+    case 'b':
+      answer = "bird";
+      break;
+    case 'c':
+      answer = "cat";
+      break;
+    default: 
+      answer = "stuff";
+      break;
+  }
+  return answer;
+}
+switchOfStuff(1);
