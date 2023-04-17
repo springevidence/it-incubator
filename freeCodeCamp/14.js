bea541171b79879d5e67b88eee769d6c8044ae38
springevidence <exshipbuilder1@gmail.com>
2023-04-17 10:38:23 +0300

    fcc | done nested arrays task

--- freeCodeCamp/14.js
+++ freeCodeCamp/14.js
@@ -118,3 +118,24 @@ const myStorage = {
     }
 };
 const gloveBoxContents = myStorage.car.inside["glove box"];
+
+//Accessing Nested Arrays
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+];
+const secondTree = myPlants[1].list[1];
