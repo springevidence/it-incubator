0458ed9ec96fef82a2dabed1f3cca0cb5506d7e2
springevidence <exshipbuilder1@gmail.com>
2023-04-18 20:20:53 +0300

    fcc | done nesting for loops task

--- freeCodeCamp/16.js
+++ freeCodeCamp/16.js
@@ -26,9 +26,20 @@ for (let i = 9; i >= 1; i -= 2) {
 }
 
 // Iterate Through an Array with a For Loop
-// Setup
 const myArr = [2, 3, 4, 5, 6];
 let total = 0;
 for (let i = 0; i < myArr.length; i++) {
     total += myArr[i];
 }
+
+// Nesting For Loops
+function multiplyAll(arr) {
+    let product = 1;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            product = product * arr[i][j];
+      }
+    }
+    return product;
+}
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
