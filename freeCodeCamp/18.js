a1e84e371139c6be23b835d44cfa71ec599fa500
springevidence <exshipbuilder1@gmail.com>
2023-04-20 20:34:54 +0300

    fcc | done using parseInt func with radix task

--- freeCodeCamp/18.js
+++ freeCodeCamp/18.js
@@ -19,3 +19,11 @@ function convertToInteger(str) {
   return parseInt(str);
 }
 convertToInteger("56");
+
+//Use the parseInt Function with a Radix
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+convertToInteger("10011");
+
+//
