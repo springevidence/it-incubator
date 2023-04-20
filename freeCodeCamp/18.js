4e18d59ab4591c55a669a904460f02ad53f6421e
springevidence <exshipbuilder1@gmail.com>
2023-04-20 11:17:36 +0300

    fcc | done generate random whole number task

--- freeCodeCamp/18.js
+++ freeCodeCamp/18.js
@@ -3,4 +3,9 @@ function randomFraction() {
   return Math.random();
 }
 
+// Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+  return  Math.floor(Math.random() * 10);
+}
+
 //
