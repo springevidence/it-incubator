ff68a5428a598ad2862545ac769ca336a41395df
springevidence <exshipbuilder1@gmail.com>
2023-04-20 20:32:39 +0300

    fcc | done using parseInt function task

--- freeCodeCamp/18.js
+++ freeCodeCamp/18.js
@@ -13,3 +13,9 @@ function randomRange(myMin, myMax) {
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 }
 randomRange(3, 7);
+
+// Use the parseInt Function
+function convertToInteger(str) {
+  return parseInt(str);
+}
+convertToInteger("56");
