cacfef5c54bf428e81a471ca974ca990eb8295dd
springevidence <exshipbuilder1@gmail.com>
2023-04-23 11:13:19 +0300

    fcc | done using recursion task

--- freeCodeCamp/19.js
+++ freeCodeCamp/19.js
@@ -1,1 +1,11 @@
 // Use Recursion to Create a Countdown
+function countdown(n){
+    if (n < 1) {
+      return [];
+    } else {
+      const constArray = countdown(n - 1);
+      constArray.unshift(n);
+      return constArray;
+    }
+}
+countdown(5);
